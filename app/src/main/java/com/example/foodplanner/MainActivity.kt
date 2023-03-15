cf084db7253996847ee9970fa54f7add26a77ef4
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-15 19:58:18 +0100

    Added filter to bar menu

--- app/src/main/java/com/example/foodplanner/MainActivity.kt
+++ app/src/main/java/com/example/foodplanner/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.foodplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -23,5 +26,17 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.recipesFragment, R.id.calendarFragment, R.id.groceryListFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miFilter -> Toast.makeText(this, "You clicked on Filter", Toast.LENGTH_LONG).show()
+        }
+        return true
     }
 }
