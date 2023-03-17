99593c0d54867c9e6c738a99b57e930c0170eaea
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-17 20:12:17 +0100

    Added filter button to RecipeFragment and added filter dialog with toggle buttons

--- app/src/main/java/com/example/foodplanner/MainActivity.kt
+++ app/src/main/java/com/example/foodplanner/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.foodplanner
 
-import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -41,42 +35,5 @@ class MainActivity : AppCompatActivity() {
         // Set up the BottomNavigationView with the NavController
         bottomNavigationView.setupWithNavController(navController)
     }
-
-    // Inflate the menu action bar for the ActionBar
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_bar_menu, menu)
-        return true
-    }
-
-    // Handle clicks on the menu items in the ActionBar
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.miFilter) {
-            val filterOptions = arrayOf("Hungarian", "Swedish", "Middle Eastern")
-            val selectedFilters = booleanArrayOf(false, false, false)
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Select Filters")
-            builder.setMultiChoiceItems(filterOptions, selectedFilters) { dialog, which, isChecked ->
-                selectedFilters[which] = isChecked
-                val btn = (dialog as AlertDialog).listView.getChildAt(which)
-                if (isChecked) {
-                    btn.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.teal_700))
-                } else {
-                    btn.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.teal_200))
-                }
-            }
-            builder.setPositiveButton("Filter") { dialog, which ->
-                Toast.makeText(this, "Applying the filter", Toast.LENGTH_SHORT).show()
-                // Perform the filtering action using the selected filters
-            }
-            builder.setNegativeButton("Cancel") { dialog, which ->
-                Toast.makeText(this, "No filter options were made", Toast.LENGTH_SHORT).show()
-                // Perform any necessary cleanup or default behavior
-            }
-            builder.show()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 
 }
--- app/src/main/java/com/example/foodplanner/RecipesFragment.kt
+++ app/src/main/java/com/example/foodplanner/RecipesFragment.kt
@@ -1,10 +1,13 @@
 package com.example.foodplanner
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import data.RecipeData
 
@@ -18,12 +21,13 @@ private const val ARG_PARAM2 = "param2"
  * Use the [RecipesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), MenuProvider {
+
+    private var filteredRecipes: List<Recipe> = RecipeData.recipes
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +35,22 @@ class RecipesFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Get a reference to the parent activity that implements the MenuHost interface
+        val menuHost: MenuHost = requireActivity()
+
+        // Add this fragment as a menu provider to the parent activity
+        // This allows the fragment to handle menu-related events
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recipes, container, false)
     }
 
@@ -50,6 +64,59 @@ class RecipesFragment : Fragment() {
         val recyclerViewRecipes = view.findViewById<RecyclerView>(R.id.recyclerViewRecipes)
         recyclerViewRecipes.adapter = RecipeAdapter(recipes, requireContext())
 
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+
+        // Get a reference to the filter menu item
+        val filterMenuItem = menu.findItem(R.id.miFilter)
+
+        // Set the click listener for the filter menu item
+        filterMenuItem.setOnMenuItemClickListener {
+
+            // Show the filter dialog
+            showFilterDialog()
+
+            // Return true to indicate that the click event has been handled
+            true
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
+    }
+
+    private fun showFilterDialog() {
+
+        // Create a new AlertDialog Builder
+        val builder = AlertDialog.Builder(requireContext())
+
+        // Set the dialog title
+        builder.setTitle("Filter Recipes")
+
+        // Get the layout inflater
+        val inflater = requireActivity().layoutInflater
+
+        // Inflate the dialog view
+        val dialogView = inflater.inflate(R.layout.dialog_filter, null)
+
+        // Add the dialog view to the builder
+        builder.setView(dialogView)
+
+        // Set the positive button click listener
+        builder.setPositiveButton("Filter") { _, _ ->
+            // Show a toast when the filter button is clicked
+            Toast.makeText(requireContext(), "Applying filter", Toast.LENGTH_SHORT).show()
+        }
+
+        // Set the negative button click listener
+        builder.setNegativeButton("Cancel") { _, _ ->
+            // Show a toast when the cancel button is clicked
+            Toast.makeText(requireContext(), "No filter applied", Toast.LENGTH_SHORT).show()
+        }
+        // Show the dialog
+        builder.show()
     }
 
     companion object {
@@ -71,4 +138,5 @@ class RecipesFragment : Fragment() {
                 }
             }
     }
+
 }
