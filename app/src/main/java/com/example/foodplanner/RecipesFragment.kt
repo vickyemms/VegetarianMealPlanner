4166e6a6c576cc50b29ae7c1bef5955384bf4880
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-03-21 19:52:31 +0100

    Implemented filter functionality in RecipesFragment.kt

--- app/src/main/java/com/example/foodplanner/RecipesFragment.kt
+++ app/src/main/java/com/example/foodplanner/RecipesFragment.kt
@@ -3,6 +3,7 @@ package com.example.foodplanner
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
@@ -24,7 +25,14 @@ private const val ARG_PARAM2 = "param2"
 
 class RecipesFragment : Fragment(), MenuProvider {
 
-    //private var filteredRecipes: List<Recipe> = RecipeData.recipes
+    // Get the list of recipes from RecipeData
+    private val recipes = RecipeData.recipes
+
+    //Declare recyclerViewRecipes
+    private lateinit var recyclerViewRecipes: RecyclerView
+
+    // Define a variable to hold the filtered list of recipes
+    private var filteredRecipes: List<Recipe> = listOf()
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -36,7 +44,6 @@ class RecipesFragment : Fragment(), MenuProvider {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onCreateView(
@@ -58,11 +65,9 @@ class RecipesFragment : Fragment(), MenuProvider {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Get the list of recipes from RecipeData
-        val recipes = RecipeData.recipes
-
-        // Find the RecyclerView in the layout and set its adapter to a new instance of RecipeAdapter
-        val recyclerViewRecipes = view.findViewById<RecyclerView>(R.id.recyclerViewRecipes)
+        //Find recycler view in the layout
+        recyclerViewRecipes = view.findViewById(R.id.recyclerViewRecipes)
+        // Set recycler views adapter to a new instance of RecipeAdapter
         recyclerViewRecipes.adapter = RecipeAdapter(recipes, requireContext())
 
     }
@@ -105,9 +110,67 @@ class RecipesFragment : Fragment(), MenuProvider {
         // Add the dialog view to the builder
         builder.setView(dialogView)
 
+        // Get references to the filter UI components
+        val tbAsian = dialogView.findViewById<ToggleButton>(R.id.tbAsian)
+        val tbMexican = dialogView.findViewById<ToggleButton>(R.id.tbMexican)
+        val tbMiddleEastern = dialogView.findViewById<ToggleButton>(R.id.tbMiddleEastern)
+        val tbMediterranean = dialogView.findViewById<ToggleButton>(R.id.tbMediterranean)
+        val tbSwedish = dialogView.findViewById<ToggleButton>(R.id.tbSwedish)
+        val tbHungarian = dialogView.findViewById<ToggleButton>(R.id.tbHungarian)
+        val tbItalian = dialogView.findViewById<ToggleButton>(R.id.tbItalian)
+        val tbAmerican = dialogView.findViewById<ToggleButton>(R.id.tbAmerican)
+        val tbChickpeas = dialogView.findViewById<ToggleButton>(R.id.tbChickpeas)
+        val tbBeans = dialogView.findViewById<ToggleButton>(R.id.tbBeans)
+        val tbLentils = dialogView.findViewById<ToggleButton>(R.id.tbLentils)
+        val tbTofu = dialogView.findViewById<ToggleButton>(R.id.tbTofu)
+        val tbFalafel = dialogView.findViewById<ToggleButton>(R.id.tbFalafel)
+        val tbSoyProtein = dialogView.findViewById<ToggleButton>(R.id.tbSoyProtein)
+        val tbPeaProtein = dialogView.findViewById<ToggleButton>(R.id.tbPeaProtein)
+        val tbNoProtein = dialogView.findViewById<ToggleButton>(R.id.tbNoProtein)
+        val tbHealthy = dialogView.findViewById<ToggleButton>(R.id.tbHealthy)
+        val tbNeutral = dialogView.findViewById<ToggleButton>(R.id.tbNeutral)
+        val tbUnhealthy = dialogView.findViewById<ToggleButton>(R.id.tbUnhealthy)
+
+
         // Set the positive button click listener
-        builder.setPositiveButton("Filter") { _, _ ->
-            // Show a toast when the filter button is clicked
+        builder.setPositiveButton("Apply") { _, _ ->
+
+            // Get the selected filter options
+            val isAsian = tbAsian.isChecked
+            val isMexican = tbMexican.isChecked
+            val isMiddleEastern = tbMiddleEastern.isChecked
+            val isMediterranean = tbMediterranean.isChecked
+            val isSwedish = tbSwedish.isChecked
+            val isHungarian = tbHungarian.isChecked
+            val isItalian = tbItalian.isChecked
+            val isAmerican = tbAmerican.isChecked
+            val hasChickpeas = tbChickpeas.isChecked
+            val hasBeans = tbBeans.isChecked
+            val hasLentils = tbLentils.isChecked
+            val hasTofu = tbTofu.isChecked
+            val hasFalafel = tbFalafel.isChecked
+            val hasSoyProtein = tbSoyProtein.isChecked
+            val hasPeaProtein = tbPeaProtein.isChecked
+            val hasNoProtein = tbNoProtein.isChecked
+            val isHealthy = tbHealthy.isChecked
+            val isNeutral = tbNeutral.isChecked
+            val isUnhealthy = tbUnhealthy.isChecked
+
+            // Filter the recipe list based on the selected options
+            filteredRecipes = recipes.filter { recipe ->
+                shouldIncludeRecipe(recipe, isAsian, isMexican, isMiddleEastern, isMediterranean,
+                    isSwedish, isHungarian, isItalian, isAmerican, hasChickpeas, hasLentils,
+                    hasBeans, hasTofu, hasSoyProtein, hasPeaProtein, hasNoProtein, hasFalafel,
+                    isHealthy, isNeutral, isUnhealthy)
+            }
+
+            //Update recipes
+            recyclerViewRecipes.adapter = RecipeAdapter(filteredRecipes, requireContext())
+
+            // Notify the RecyclerView adapter that the recipe list has changed
+            recyclerViewRecipes.adapter?.notifyDataSetChanged()
+
+            // Show a toast to indicate that the filter has been applied
             Toast.makeText(requireContext(), "Applying filter", Toast.LENGTH_SHORT).show()
         }
 
@@ -116,8 +179,33 @@ class RecipesFragment : Fragment(), MenuProvider {
             // Show a toast when the cancel button is clicked
             Toast.makeText(requireContext(), "No filter applied", Toast.LENGTH_SHORT).show()
         }
+
         // Show the dialog
         builder.show()
+    }
+
+    //Determines whether a given recipe should be included in the filtered list based on the provided
+    private fun shouldIncludeRecipe(recipe: Recipe, isAsian: Boolean, isMexican: Boolean, isMiddleEastern: Boolean, isMediterranean: Boolean, isSwedish: Boolean, isHungarian: Boolean, isItalian: Boolean, isAmerican: Boolean, hasChickpeas: Boolean, hasLentils: Boolean, hasBeans: Boolean, hasTofu: Boolean, hasSoyProtein: Boolean, hasPeaProtein: Boolean, hasNoProtein: Boolean, hasFalafel: Boolean, isHealthy: Boolean, isNeutral: Boolean, isUnhealthy: Boolean): Boolean {
+        if (isAsian && recipe.cuisine != "Asian") return false
+        if (isMexican && recipe.cuisine != "Mexican") return false
+        if (isMiddleEastern && recipe.cuisine != "Middle Eastern") return false
+        if (isMediterranean && recipe.cuisine != "Mediterranean") return false
+        if (isSwedish && recipe.cuisine != "Swedish") return false
+        if (isHungarian && recipe.cuisine != "Hungarian") return false
+        if (isItalian && recipe.cuisine != "Italian") return false
+        if (isAmerican && recipe.cuisine != "American") return false
+        if (hasChickpeas && recipe.protein != "Chickpeas") return false
+        if (hasLentils && recipe.protein != "Lentils") return false
+        if (hasBeans && recipe.protein != "Beans") return false
+        if (hasTofu && recipe.protein != "Tofu") return false
+        if (hasSoyProtein && recipe.protein != "Soy protein") return false
+        if (hasPeaProtein && recipe.protein != "Pea protein") return false
+        if (hasNoProtein && recipe.protein != "No protein") return false
+        if (hasFalafel && recipe.protein != "Falafel") return false
+        if (isHealthy && recipe.healthiness != "Healthy") return false
+        if (isNeutral && recipe.healthiness != "Neutral") return false
+        if (isUnhealthy && recipe.healthiness != "Unhealthy") return false
+        return true
     }
 
     companion object {
